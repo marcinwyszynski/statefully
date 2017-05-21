a9528ab1150c3196443185816fc65cb64175f636
Marcin Wyszynski <marcin.pixie@gmail.com>
2017-05-21 10:18:20 +0200

    [feature] Support early return using Finished (#4)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,11 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'pry'
+
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 require 'statefully'
--- spec/state_spec.rb
+++ spec/state_spec.rb
@@ -33,21 +33,29 @@ module Statefully
       it { expect(subject.resolve).to eq subject }
       it { expect(subject).to be_success }
       it { expect(subject).not_to be_failure }
+      it { expect(subject).not_to be_finished }
     end # describe 'trivial readers'
 
-    describe '#succeed' do
-      let(:new_val) { 'new_val' }
-      let(:succeeded) { subject.succeed(new_key: new_val) }
+    shared_examples 'successful_state' do
+      it { expect(next_state).to be_success }
+      it { expect(next_state.old_key).to eq val }
 
-      it { expect(succeeded).to be_success }
-      it { expect(succeeded.old_key).to eq val }
-      it { expect(succeeded.new_key).to eq new_val }
-      it { expect(succeeded.keys).to eq %i[old_key new_key] }
-      it { expect(succeeded.previous).to eq subject }
-      it { expect(succeeded.resolve).to eq succeeded }
+      it { expect(next_state.previous).to eq subject }
+      it { expect(next_state.resolve).to eq next_state }
+    end # shared_examples 'successful_state'
+
+    describe '#succeed' do
+      let(:new_val)    { 'new_val' }
+      let(:next_state) { subject.succeed(new_key: new_val) }
+
+      it_behaves_like 'successful_state'
+
+      it { expect(next_state).not_to be_finished }
+      it { expect(next_state.new_key).to eq new_val }
+      it { expect(next_state.keys).to eq %i[old_key new_key] }
 
       context 'with history' do
-        let(:history) { succeeded.history }
+        let(:history) { next_state.history }
 
         it { expect(history.size).to eq 2 }
         it { expect(history.first.added).to include :new_key }
@@ -56,28 +64,47 @@ module Statefully
     end # describe '#succeed'
 
     describe '#fail' do
-      let(:error)  { RuntimeError.new('snakes on a plane') }
-      let(:failed) { subject.fail(error) }
+      let(:error)      { RuntimeError.new('snakes on a plane') }
+      let(:next_state) { subject.fail(error) }
 
-      it { expect(failed).not_to be_success }
-      it { expect(failed).to be_failure }
-      it { expect(failed.old_key).to eq val }
-      it { expect(failed.previous).to eq subject }
-      it { expect(failed.error).to eq error }
+      it { expect(next_state).not_to be_success }
+      it { expect(next_state).to be_failure }
+      it { expect(next_state).not_to be_finished }
+      it { expect(subject).not_to be_finished }
+      it { expect(next_state.old_key).to eq val }
+      it { expect(next_state.previous).to eq subject }
+      it { expect(next_state.error).to eq error }
 
       it 'raises passed error on #resolve' do
-        expect { failed.resolve }.to raise_error do |err|
+        expect { next_state.resolve }.to raise_error do |err|
           expect(err).to eq error
         end
       end
 
       context 'with history' do
-        let(:history) { failed.history }
+        let(:history) { next_state.history }
 
         it { expect(history.size).to eq 2 }
         it { expect(history.first).to eq error }
         it { expect(history.last.added).to include :old_key }
       end # context 'with history'
     end # describe '#fail'
+
+    describe '#finish' do
+      let(:new_val)    { 'new_val' }
+      let(:next_state) { subject.finish }
+
+      it_behaves_like 'successful_state'
+
+      it { expect(next_state).to be_finished }
+
+      context 'with history' do
+        let(:history) { next_state.history }
+
+        it { expect(history.size).to eq 2 }
+        it { expect(history.first).to eq :finished }
+        it { expect(history.last.added).to include :old_key }
+      end # context 'with history'
+    end # describe '#finish'
   end # describe 'State::Success'
 end # module Statefully
